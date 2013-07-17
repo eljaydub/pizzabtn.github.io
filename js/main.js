6dd55a76d7d0b91cce1eceae71945ed0b4012d13
Shchukin Anton <cypher76@gmail.com>
2013-07-17 18:34:47 +0400

    Just another big version
    looking for content for now

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $('.slider__item').first().addClass('slider__item_first');
+    $('.slider__item').last().addClass('slider__item_last');
+
+    function slideChange() {
+
+        if( ! $('.slider__item_current').hasClass('slider__item_last') ) {
+            $('.slider__item_current').next().fadeIn(function(){
+                $(this).addClass('slider__item_current');
+            });
+        }
+        else {
+            $('.slider__item_first').fadeIn(function(){
+                $(this).addClass('slider__item_current');
+            });
+        }
+
+        $('.slider__item_current').fadeOut(function(){
+            $(this).removeClass('slider__item_current');
+        });
+
+    }
+
+    var slider = setInterval(slideChange, 5000);
+
+
+
+
+/*
+    $('.logo').on('click', function(){
+        clearInterval(slider);
+    });
+
+    $('.ribbon').on('click', function(){
+        clearInterval(slider);
+        slider = setInterval(slideChange, 2000)
+    });
+*/
+
+});
