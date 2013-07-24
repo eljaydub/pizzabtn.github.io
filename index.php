97b2b637dfacf6ac6a7a2c3ede1190a1645ae530
Shchukin Anton <cypher76@gmail.com>
2013-07-24 18:02:49 +0400

    Just another pizza site

---
+++ index.php
@@ -0,0 +1,134 @@
+<?php include 'functions.php'; ?>
+<!DOCTYPE html>
+<html class="<?php echo getOS();?> <?php echo getBrowser(); ?> no-js" lang="ru">
+    <head>
+
+        <meta charset="utf-8">
+        <title>PizzaButton</title>
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+
+    <body>
+
+
+        <div class="page">
+
+
+            <div class="page__slide page__slide_pizza">
+                <div class="slide-pizza">
+                    <div class="slide-pizza__logo">
+                        <div class="logo"></div>
+                    </div>
+                    <div class="slide-pizza__ribbon">
+                        <h1 class="ribbon">
+                            SDK to sell pizza inside your apps
+                            &nbsp;
+                            <strong>Up to 10% from every order</strong>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page__slide">
+                <div class="region">
+                    <div class="slide-video">
+                        <div class="slide-video__title">
+                            <h2 class="">What is it?</h2>
+                        </div>
+                        <div class="slide-video__video">
+                            <div class="video video_bordered">
+                                <iframe class="video__frame" src="http://player.vimeo.com/video/69647216?api=1" autoplay="1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                            </div>
+                        </div>
+                        <div class="slide-video__button">
+                            <a class="button" href="http://pizzabtn.herokuapp.com/admin">Join</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="page__slide">
+                <div class="region">
+                    <div class="slide-setup">
+                        <div class="slide-setup__screenshot">
+                            <div class="iphone">
+                                <div class="screen-shot screen-shot_setup-button"></div>
+                            </div>
+                        </div>
+                        <div class="slide-setup__line"></div>
+                        <div class="slide-setup__text">
+                            <div class="text">
+                                <h2 class="text__title">You integrate Pizzabtn SDK</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page__slide">
+                <div class="region">
+                    <div class="slide-push">
+                        <div class="slide-push__screenshot">
+                            <div class="iphone">
+                                <div class="screen-shot screen-shot_push-button"></div>
+                            </div>
+                        </div>
+                        <div class="slide-push__text">
+                            <div class="text">
+                                <h2 class="text__title">Your users interact<br>with floating button</h2>
+                                <div class="text__body">Pizzabtn promote goods and<br>services that people want to buy.<br>Pizza or food delivery right now.<br>Taxi, tickets, surveys are coming.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page__slide">
+                <div class="region">
+                    <div class="slide-profit">
+                        <div class="slide-profit__screenshot">
+                            <div class="iphone">
+                                <div class="screen-shot screen-shot_profit"></div>
+                            </div>
+                        </div>
+                        <div class="slide-profit__line"></div>
+                        <div class="slide-profit__text">
+                            <div class="text">
+                                <h2 class="text__title">Profit!</h2>
+                                <div class="text__body">Get up to 10% of the order value.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page__footer">
+                <div class="region">
+                    <div class="footer">
+                        <div class="footer__copyright">
+                            <a class="copyright" href="http://pizzabtn.herokuapp.com/admin">
+                                <span class="copyright__title">©</span>
+                                <span class="copyright__text">Pizzabtn</span>
+                            </a>
+                        </div>
+                        <div class="footer__social"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- /page -->
+
+
+
+
+
+        <!-- Scripts -->
+        <script src="js/main.js"></script>
+        <!--/Scripts -->
+
+
+    </body>
+</html>
--- sass/config.rb
+++ sass/config.rb
@@ -4,7 +4,7 @@ dir = File.dirname(__FILE__)
 sass_path = dir
 css_path = File.join(dir, "..", "css")
 
-output_style = :expanded
-environment = :developmen
-#output_style = :compressed
-#environment = :production
+# output_style = :expanded
+# environment = :developmen
+output_style = :compressed
+environment = :production
